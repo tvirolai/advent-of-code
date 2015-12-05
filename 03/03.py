175941ceda9b97e705a3006aa0ac9cb40d21b419
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2015-12-05 22:25:14 +0200

    03 2

--- 03/03.py
+++ 03/03.py
@@ -5,24 +5,34 @@ class SantaTrip(object):
         with open("input.txt", "r") as f:
             self.data = list(f.read())
         self.location = (0, 0)
-        self.visited = [(0, 0)]
+        self.robotLocation = (0, 0)
+        self.visited = [self.location]
 
-    def houseCount(self):
+    def partOne(self):
         for instruction in self.data:
-            self.move(instruction)
+            self.location = self.move(instruction, self.location)
         print(len(set(self.visited)))
 
-    def move(self, instruction):
+    def partTwo(self):
+        for i, instruction in enumerate(self.data):
+            if i % 2 == 0:
+                self.location = self.move(instruction, self.location)
+            else:
+                self.robotLocation = self.move(instruction, self.robotLocation)
+        print(len(set(self.visited)))
+
+    def move(self, instruction, location):
         if instruction == "^":
-            self.location = (self.location[0], self.location[1] + 1)
+            location = (location[0], location[1] + 1)
         elif instruction == "v":
-            self.location = (self.location[0], self.location[1] - 1)
+            location = (location[0], location[1] - 1)
         elif instruction == ">":
-            self.location = (self.location[0] + 1, self.location[1])
+            location = (location[0] + 1, location[1])
         elif instruction == "<":
-            self.location = (self.location[0] - 1, self.location[1]) 
-        self.visited.append(self.location)
+            location = (location[0] - 1, location[1]) 
+        self.visited.append(location)
+        return location
 
 if __name__ == "__main__":
   trip = SantaTrip()
-  trip.houseCount()
+  trip.partTwo()
