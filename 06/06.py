b5cc15087bbcb04ff37fc2b24a877df23dfe09f8
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2015-12-06 16:33:19 +0200

    06 / 1

---
+++ 06/06.py
@@ -0,0 +1,58 @@
+#!/bin/env python
+
+
+class LightControl(object):
+
+    def __init__(self):
+        with open("input.txt", "r") as f:
+            self.data = f.read().split("\n")
+            self.data = [x for x in self.data if len(x) > 5]
+        self.lightsOn = []
+
+    def read(self):
+        for i, line in enumerate(self.data):
+            print("Processing line {0} / {1}".format(i, len(self.data)))
+            self.processCommand(line)
+        print(len(self.lightsOn))
+
+    def processCommand(self, line):
+        instructions = self.processLine(line)
+        points = self.returnAllPoints(
+            instructions["point1"], instructions["point2"])
+        if (instructions["command"] == "on"):
+            self.lightsOn = self.lightsOn + points
+        elif (instructions["command"] == "off"):
+            self.lightsOn = list(set(self.lightsOn) - set(points))
+        elif (instructions["command"] == "toggle"):
+            notInList = set(points) - (set(self.lightsOn) & set(points))
+            inList = set(self.lightsOn) & set(points)
+            self.lightsOn = list(set(self.lightsOn + list(notInList)) - inList)
+
+    def returnAllPoints(self, point1, point2):
+        allPoints = []
+        cursor = point1
+        while True:
+            allPoints.append(cursor)
+            while cursor[0] < point2[0]:
+                cursor = (cursor[0] + 1, cursor[1])
+                allPoints.append(cursor)
+            if (cursor == point2):
+                break
+            else:
+                cursor = (point1[0], cursor[1] + 1)
+        return allPoints
+
+    def processLine(self, line):
+        if not "toggle" in line:
+            _, command, first, _, second = line.split(" ")
+        else:
+            command, first, _, second = line.split(" ")
+        return {
+            "command": command,
+            "point1": (int(first.split(",")[0]), int(first.split(",")[1])),
+            "point2": (int(second.split(",")[0]), int(second.split(",")[1]))
+        }
+
+if __name__ == "__main__":
+    lc = LightControl()
+    lc.read()
