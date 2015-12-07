9e5f1bed37f4c0ba28b7bedf4c1f0af321ca5092
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2015-12-07 23:58:55 +0200

    07/1

---
+++ 07/07_1.py
@@ -0,0 +1,52 @@
+class BitwiseLogic(object):
+    def __init__(self):
+        with open('input.txt', 'r') as f:
+            self.data = [x for x in f.read().split("\n") if len(x) > 5]
+        self.wires = {}
+
+    def processLine(self, line):
+        wire = line.split(" -> ")[1]
+        if len(line.split(" ")) == 3:
+            operand = line.split(" -> ")[0]
+            try:
+                operand = int(operand)
+                self.wires[wire] = operand
+            except ValueError as e:
+                try:
+                    self.wires[wire] = self.wires[operand]
+                except:
+                    pass
+        elif "AND" in line:
+            op1, op2 = line.split(" -> ")[0].split(" AND ")
+            if (op1 in self.wires and op2 in self.wires):
+                self.wires[wire] = self.wires[op1] & self.wires[op2]
+            try:
+                op1 = int(op1)
+                self.wires[wire] = op1 & self.wires[op2]
+            except:
+                pass
+        elif "OR" in line:
+            op1, op2 = line.split(" -> ")[0].split(" OR ")
+            if (op1 in self.wires and op2 in self.wires):
+                self.wires[wire] = self.wires[op1] | self.wires[op2]
+        elif "LSHIFT" in line:
+            op1, number = line.split(" -> ")[0].split(" LSHIFT ")
+            if (op1 in self.wires):
+                self.wires[wire] = self.wires[op1] << int(number)
+        elif "RSHIFT" in line:
+            op1, number = line.split(" -> ")[0].split(" RSHIFT ")
+            if (op1 in self.wires):
+                self.wires[wire] = self.wires[op1] >> int(number)
+        elif "NOT" in line:
+            op1 = line.split(" -> ")[0].split(" ")[1]
+            if (op1 in self.wires):
+                self.wires[wire] = ~ self.wires[op1] & 0xffff
+
+    def loop(self):
+        while "a" not in self.wires:
+            for line in self.data:
+                self.processLine(line)
+        print(self.wires["a"])
+
+bl = BitwiseLogic()
+bl.loop()
