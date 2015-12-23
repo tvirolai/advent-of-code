e0e6f329f11db982a079c06a311d2ec614ba65f6
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-12-23 12:52:42 +0200

    17

--- 10/test2.py
+++
@@ -1,9 +0,0 @@
-from itertools import groupby
-
-def test(string):
-  splitString = []
-  for x in [list(g) for k, g in groupby(string)]:
-    splitString.append("".join(x))
-  return splitString
-
-print(test("1122455"))
