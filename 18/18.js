3472c4317b34971b3357bed951973d4e78de3127
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-01-05 14:09:19 +0200

    18 progress...

--- 18/18.js
+++ 18/18.js
@@ -9,10 +9,11 @@ const _ = require("underscore");
 fs.readFile("input.txt", "utf-8", (err, data) => {
   const matrix = _.map(_.filter(data.split("\n"), (d) => d.length > 0), (list) => list.split(""));
   console.log(neighboursOn([0, 1], matrix));
+  console.log(isOn([0, 3], matrix));
 });
 
 function getNeighbours(point) {
-  // The function takes and returns an array. Pretty funky lookin', I know.
+  // The function takes and returns an array. Pretty funky lookin'?
   const permutations = _.map(_.range(point[0] - 1, point[0] + 2), (item) => {
     return _.map(_.range(point[1] - 1, point[1] + 2), (item2) => [item, item2]);
   });
@@ -21,4 +22,8 @@ function getNeighbours(point) {
 
 function neighboursOn(point, matrix) {
   return _.filter(_.map(getNeighbours(point), (item) => matrix[item[0]][item[1]] === "#"), (item) => item === true).length;
+}
+
+function isOn(point, matrix) {
+  return matrix[point[0]][point[1]] === "#";
 }
