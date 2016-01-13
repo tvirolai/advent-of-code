9dbcdad0748bc860ef0dfc5ff5cbdd10fec47aac
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-01-13 13:20:40 +0200

    Even shorter solution

--- 01/01.py
+++ 01/01.py
@@ -2,9 +2,7 @@
 
 
 def accumulate(values, total=0):
-    for x in values:
-        total += x
-        yield total
+    return (x + sum(values[:i]) for i, x in enumerate(values))
 
 with open("input.txt", "r") as f:
     data = map(lambda x: 1 if x == "(" else -1, list(f.read()))
