dcf48d6cb5bcae082df02ebf563fad6e1fdb918d
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-02-25 17:35:15 +0200

    12 (2/2) still underway

--- 12/12.js
+++ 12/12.js
@@ -3,11 +3,12 @@
 "use strict";
 
 const fs = require("fs");
-const _ = require("lodash");
+const _ = require("underscore");
 
 fs.readFile("input.txt", "utf-8", (err, data) => {
-  part1(data);
+  //part1(data);
   part2(data);
+  //print(data);
 });
 
 function part1(data) {
@@ -17,6 +18,92 @@ function part1(data) {
 }
 
 function part2(data) {
-  let dataWithoutRed = data.replace(/{(.)*red(.)*}/g, " ");
-  console.log(dataWithoutRed)
+  let processedData = "";
+  data = JSON.stringify(data);
+  for (let i = 0; i < data.length; i++) {
+    if (data.substr(i, 3) === "red") {
+      processedData += "red";
+    } else if (data[i].match(/[-|\d|\{|\}|\[|\]]/)) {
+      processedData += data[i];
+    } else {
+      processedData += " ";
+    }
+  }
+  processedData = processedData.replace(/\s+/g, " ");
+  removeRedObjects(processedData);
+}
+
+function removeRedObjects(string) {
+  let data = string;
+  let redI = data.indexOf("red");
+  console.log(redI);
+  console.log(data.length);
+  if (redI === -1) {
+    return data;
+  }
+  let parenthesis = {
+    "openingIndex": 0,
+    "closingIndex": 0,
+    "curly": 0
+  };
+
+  // Find opening parenthesis ("[" or "{")
+  for (let i = redI; i > 0; i--) {
+    if (data[i] === "{") {
+      parenthesis.openingIndex = i;
+      parenthesis.curly = 1;
+      break;
+    } else if (data[i] === "[") {
+      parenthesis.openingIndex = i;
+      break;
+    }
+  }
+
+  // Find closing parenthesis ("]" or "}")
+  let closing = "";
+  if (parenthesis.curly === 1) {
+    closing = "}";
+  } else {
+    closing = "]";
+  }
+
+  let curlyCount = 0;
+  let parenthesisCount = 0;
+
+  for (let i = parenthesis.openingIndex; i < data.length - parenthesis.openingIndex; i++) {
+    if (data[i] === "{") {
+      ++curlyCount;
+    } else if (data[i] === "[") {
+      ++parenthesisCount;
+    } else if (data[i] === "]") {
+      --parenthesisCount;
+    } else if (data[i] === "}") {
+      --curlyCount;
+    }
+    console.log("Curly: " + curlyCount);
+    console.log("parenthesis: " + parenthesisCount);
+    if (curlyCount === 0 && parenthesis.curly === 1) {
+      parenthesis.closingIndex = i;
+      break;
+    } else if (parenthesisCount === 0 && parenthesis.curly === 0) {
+      parenthesis.closingIndex = i;
+      break;
+    }
+  }
+
+  if (parenthesis.closingIndex === 0) {
+    console.log(data);
+    console.log("Done!");
+    return data;
+  }
+
+  // If an object with "red" was found, it is deleted
+  if (parenthesis.curly === 1 && parenthesis.closingIndex > 0) {
+    data = data.substr(0, parenthesis.openingIndex) + data.substr(parenthesis.closingIndex + 1);
+    //console.log(data.length);
+    //console.log(parenthesis);
+
+  } else if (parenthesis.curly === 0 && parenthesis.closingIndex > 0) {
+    data = data.substr(0, redI) + data.substr(redI + 3);
+  }
 }
